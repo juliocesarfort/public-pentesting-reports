1ba0d4cc6fa45acde2c0bfc228c03a0c4d7bb0bf
ducksecops <daniel@ducksecops.uk>
2019-10-29 23:40:32 +0000

    Add files via upload
    Added public reports from Radically Open Security

---
+++ radicallyopensecurity/Second Audit Report | F-Droid - Free and Open Source Android App Repository_files/sidebar-lang.js
@@ -0,0 +1,26 @@
+//@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-v3
+
+/**
+ * When the user changes the language via the language drop down, redirect the user to the relevant translation.
+ */
+(function() {
+    var chooser = document.getElementById('language-chooser-select');
+    chooser.onchange = function() {
+        var lang = this.value;
+        var activeLang = 'en';
+        var pageUrl = '/';
+
+        /*
+         * If we are already on "/fr" and select "ES", we don't want to redirect to "/fr/es".
+         * The apache docs website achieves this via AliasMatch in their apache config, but that
+         * is not able to go into the .htaccess file and so makes the deployment of the site more
+         * difficult. This solves the problem in JavaScript.
+         */
+        if (new RegExp('^' + activeLang + '/').test(pageUrl)) {
+            pageUrl = pageUrl.substring(activeLang.length + 1);
+        }
+
+        document.location = '' + '/' + lang + pageUrl;
+    }
+})();
+// @license-end
